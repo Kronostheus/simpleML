7d2ce8f0489cae4b768e9714f386160d0983f5ee
Kronostheus <nuno.amaro@tecnico.ulisboa.pt>
2024-02-12 18:30:32 +0000

    mean_shift

--- unsupervised/mean_shift.py
+++ unsupervised/mean_shift.py
@@ -5,13 +5,13 @@ from utils.calculations import minkowski_distance
 
 
 class MeanShift:
-    def __init__(self, bandwidth, max_iter=300):
-        self.bandwidth = bandwidth
-        self.max_iter = max_iter
-        self.centroids = None
-        self.labels = None
+    def __init__(self: "MeanShift", bandwidth: int, max_iter: int = 300) -> None:
+        self.bandwidth: int = bandwidth
+        self.max_iter:int = max_iter
+        self.centroids: np.ndarray = None
+        self.labels: np.ndarray = None
 
-    def fit(self, samples):
+    def fit(self: "MeanShift", samples: np.ndarray) -> np.ndarray:
         """
         Mean Shift clustering algorithm using a flat kernel
         :param samples: data samples to cluster
@@ -19,22 +19,27 @@ class MeanShift:
         """
 
         # Initialize every sample as a cluster center
-        centroids = samples
+        centroids: np.ndarray = samples
 
         for _ in tqdm(range(self.max_iter)):
 
-            new_centroids = centroids.copy()
+            new_centroids: np.ndarray = centroids.copy()
 
+            centroid: np.ndarray
             for centroid in centroids:
 
                 # Check all samples within a certain radius (flat kernel)
-                in_bandwidth = {idx: sample for idx, sample in enumerate(samples) if
-                                minkowski_distance(sample, centroid, p=2) < self.bandwidth}
+                in_bandwidth: dict[int, np.ndarray] = {
+                    idx: sample
+                    for idx, sample in enumerate(samples)
+                    if minkowski_distance(sample, centroid, p=2) < self.bandwidth
+                }
 
                 # Compute the mean of the samples found as their cluster center
-                new_centroid = np.mean(list(in_bandwidth.values()), axis=0)
+                new_centroid: np.ndarray = np.mean(list(in_bandwidth.values()), axis=0)
 
                 # Modify centroid array
+                idx: int
                 for idx in in_bandwidth:
                     new_centroids[idx] = new_centroid
 
@@ -48,14 +53,16 @@ class MeanShift:
 
         return self.labels
 
-    def predict(self, samples):
+    def predict(self: "MeanShift", samples: np.ndarray) -> list[int]:
         """
         Computes Euclidean distance towards every cluster center and returns label associated with the nearest center
         :param samples: data samples to test
         :return: List of cluster labels
         """
-        return [np.argmin([minkowski_distance(centroid, sample, p=2) for centroid in self.centroids])
-                for sample in samples]
+        return [
+            np.argmin([minkowski_distance(centroid, sample, p=2) for centroid in self.centroids])
+            for sample in samples
+        ]
 
 
 if __name__ == "__main__":
