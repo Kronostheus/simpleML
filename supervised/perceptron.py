e35010bd1d9ea154f4b49ac39b2816b6ee97b4ef
Kronostheus <nuno.amaro@tecnico.ulisboa.pt>
2024-02-12 15:38:51 +0000

    perceptron

--- supervised/perceptron.py
+++ supervised/perceptron.py
@@ -3,12 +3,12 @@ import numpy as np
 
 class Perceptron:
 
-    def __init__(self, epochs=1, learning_rate=0.1):
-        self.lr = learning_rate
-        self.epochs = epochs
-        self.weights = None
+    def __init__(self: "Perceptron", epochs: int = 1, learning_rate: float = 0.1) -> None:
+        self.lr: float = learning_rate
+        self.epochs: int = epochs
+        self.weights: np.ndarray = None
 
-    def fit(self, features, y):
+    def fit(self: "Perceptron", features: np.ndarray, y: np.ndarray) -> None:
         """
         Train perceptron
         :param features: features
@@ -17,17 +17,20 @@ class Perceptron:
         """
 
         # Add a bias column to features
-        features = self.add_bias(features)
+        features: np.ndarray = self.add_bias(features)
 
         # Initialize weights according to a uniform distribution
-        w = self.init_weights(features)
+        w: np.ndarray = self.init_weights(features)
+
+        i: int
+        x: np.ndarray
 
         for _ in range(self.epochs):
 
             for i, x in enumerate(features):
 
                 # Compute dot product between weight vector and features
-                y_pred = self.activation(np.dot(w, x))
+                y_pred: int = self.activation(np.dot(w, x))
 
                 # If we made a mistake, update weights
                 if y_pred != y[i]:
@@ -35,37 +38,37 @@ class Perceptron:
 
         self.weights = w
 
-    def predict(self, features):
+    def predict(self: "Perceptron", features: np.ndarray) -> np.ndarray:
         """
         Predicts the labels associated with features, equivalent to forward pass
         :param features: data samples
         :return: labels
         """
-        features = self.add_bias(features)
+        features: np.ndarray = self.add_bias(features)
         return np.array([self.activation(np.dot(self.weights, x)) for x in features])
 
     @staticmethod
-    def add_bias(features):
+    def add_bias(features: np.ndarray) -> np.ndarray:
         """
         Add a bias column (value = 1) to features
         :param features: original data
         :return: new data
         """
-        bias = np.ones((features.shape[0], 1))
+        bias: np.ndarray = np.ones((features.shape[0], 1))
         return np.hstack((features, bias))
 
     @staticmethod
-    def init_weights(features):
+    def init_weights(features: np.ndarray) -> np.ndarray:
         """
         Initialize weights according to a uniform distribution
         :param features: feature set (w/ bias)
         :return: weight vector
         """
-        bounds = 1.0 / np.sqrt(features.shape[0])
+        bounds: float = 1.0 / np.sqrt(features.shape[0])
         return np.random.default_rng().uniform(-bounds, bounds, size=features.shape[1])
 
     @staticmethod
-    def activation(x):
+    def activation(x: float) -> int:
         """
         Activation function
         :param x: equivalent to the dot product of weight vector and a data sample
@@ -79,20 +82,20 @@ if __name__ == "__main__":
 
 
     # Positive examples
-    P = np.array([[-2, 0], [-1, 0.5], [0, -0.1], [1, -1.5]])
+    P: np.ndarray = np.array([[-2, 0], [-1, 0.5], [0, -0.1], [1, -1.5]])
 
     # Negative examples
-    N = np.array([[2, 0], [1, 0.5], [0, 0.4], [1, 1.5]])
+    N: np.ndarray = np.array([[2, 0], [1, 0.5], [0, 0.4], [1, 1.5]])
 
-    features = np.vstack((P, N))
-    y = np.hstack((np.ones(P.shape[0]), -np.ones(N.shape[0])))
+    features: np.ndarray = np.vstack((P, N))
+    y: np.ndarray = np.hstack((np.ones(P.shape[0]), -np.ones(N.shape[0])))
 
-    clf = Perceptron(epochs=1000, learning_rate=0.01)
+    clf: Perceptron = Perceptron(epochs=1000, learning_rate=0.01)
     clf.fit(features, y)
-    preds = clf.predict(features)
+    preds: np.ndarray = clf.predict(features)
 
-    p_pred = features[np.nonzero(preds == 1)]
-    n_pred = features[np.nonzero(preds == -1)]
+    p_pred: np.ndarray = features[np.nonzero(preds == 1)]
+    n_pred: np.ndarray = features[np.nonzero(preds == -1)]
 
     fig, (ax1, ax2) = plt.subplots(2, 1)
     ax1.plot(P[:, 0], P[:, 1], 'go', label='True Positive Examples')
